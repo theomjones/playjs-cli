ce62334169fa526c72c7716e709978bd0dfbf4ef
Theo M Jones <theomjones@gmail.com>
2018-02-20 23:51:15 +0000

    pretty

--- processor.js
+++ processor.js
@@ -1,40 +1,40 @@
 module.exports = {
-    // Gives arguments back
-    filterArgs (args) {
-        return args.slice(2, args.length)
-    },
-    // Returns Array of options
-    filterOptions (args) {
-        return args.filter(arg => arg.charAt(0) === '-')
-    },
-    // Returns array of commands
-    filterCommands (args) {
-        return args.filter(arg => arg.charAt(0) !== '-')
-    },
+  // Gives arguments back
+  filterArgs(args) {
+    return args.slice(2, args.length);
+  },
+  // Returns Array of options
+  filterOptions(args) {
+    return args.filter(arg => arg.charAt(0) === "-");
+  },
+  // Returns array of commands
+  filterCommands(args) {
+    return args.filter(arg => arg.charAt(0) !== "-");
+  },
 
-    getPath (args) {
-        let path = null
-        if (args.length < 1) {
-            return undefined
-        }
-        args.forEach((arg, i) => {
-            if (arg === '--path') {
-                path = args[i + 1]
-            }
-        })
-        return path
-    },
-
-    getFileName (args) {
-        if (args.length < 1) {
-            return undefined
-        }
-        let fileName = null
-        args.forEach((arg, i) => {
-            if (arg === '--file') {
-                fileName = args[i + 1]
-            }
-        })
-        return fileName
+  getPath(args) {
+    let path = null;
+    if (args.length < 1) {
+      return undefined;
     }
-}
+    args.forEach((arg, i) => {
+      if (arg === "--path") {
+        path = args[i + 1];
+      }
+    });
+    return path;
+  },
+
+  getFileName(args) {
+    if (args.length < 1) {
+      return undefined;
+    }
+    let fileName = null;
+    args.forEach((arg, i) => {
+      if (arg === "--file") {
+        fileName = args[i + 1];
+      }
+    });
+    return fileName;
+  }
+};
